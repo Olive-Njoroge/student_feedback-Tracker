b55b5ed400137a38cef013d156863735ccd44853
Olive Njoroge <njorogeolive001@gmail.com>
2025-06-15 08:22:15 +0300

    Imported feebackform and list and added lines of code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import FeedbackForm from "@/components/FeedbackForm";
+import FeedbackList from "@/components/FeedbackList"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [feedbacks, setFeedbacks] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const addFeedback = (feedback) => {
+    setFeedbacks(f => [feedback, ...f]);
+  }
+  const deleteFeedback = (index) => {
+    setFeedbacks(prev => prev.filter((_, i) => i !== index));
+  };
+
+  return(
+    <div className="max-w-2xl mx-auto p-6 space-y-6">
+      <h1 className="text-3xl font-bold">Student Feedback</h1>
+      <FeedbackForm onAdd={addFeedback} />
+      <Feedbacklist feedbacks={feedbacks} onDelete={deleteFeedback} />
+    </div>
+  );
+ 
 }
 
 export default App
